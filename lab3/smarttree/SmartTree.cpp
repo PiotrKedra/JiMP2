bb43fdcbaa081ba535907a44a32d5d7f3e45ed73
PiotrKedra <piotr.k9750@gmail.com>
2017-03-20 20:37:11 +0100

    lab2

--- lab3/smarttree/SmartTree.cpp
+++ lab3/smarttree/SmartTree.cpp
@@ -6,6 +6,7 @@
 #include <ostream>
 #include <string>
 #include <memory>
+#include <sstream>
 
 namespace datastructures{
 
@@ -33,9 +34,54 @@ namespace datastructures{
         return tree;
     }
 
-    void PrintTreeInOrder(const std::unique_ptr<SmartTree> &unique_ptr, std::ostream *out);
+    void PrintTreeInOrder(const std::unique_ptr<SmartTree> &unique_ptr, std::ostream *out){
+        if(unique_ptr->left) PrintTreeInOrder(unique_ptr->left, out);
+        *out << std::to_string(unique_ptr->value) << ", ";
+        if(unique_ptr->right) PrintTreeInOrder(unique_ptr->right, out);
+    }
 
-    std::string DumpTree(const std::unique_ptr<SmartTree> &tree);
+    std::string DumpTree(const std::unique_ptr<SmartTree> &tree){
+        if(!tree) return "[none]";
+        else {
+            return "["+std::to_string(tree->value)+" "+DumpTree(tree->left)+" "+DumpTree(tree->right)+"]";
+        }
+    }
 
-    std::unique_ptr<SmartTree> RestoreTree(const std::string &tree);
+    std::unique_ptr<SmartTree> RestoreTree(const std::string &tree){
+        const std::string str_tree = tree;
+        std::string value;
+        std::unique_ptr<SmartTree> tree_out;
+        int right_brackets = 0;
+        int left_brackets = 0;
+        int i=0,k=0;
+
+        do{
+            if(str_tree[i] == '['){
+                i++;
+                left_brackets++;
+                if(left_brackets == 1){
+                    while(str_tree[i] != 32 and str_tree[i] != ']'){
+                        value += str_tree[i];
+                        ++i;
+                    }
+                    if(value == "none") return nullptr;
+                    tree_out = std::move(CreateLeaf(std::stoi(value)));
+                    value = "";
+                }
+                if(left_brackets == 2 and k==0){
+                    tree_out->left = std::move(RestoreTree(&(str_tree[i-1])));
+                    k=1;
+                }
+                if(left_brackets == 2 and k==1){
+                    tree_out->right = std::move(RestoreTree(&(str_tree[i-1])));
+                }
+            }
+            if(str_tree[i] == ']'){
+                left_brackets--;
+                right_brackets++;
+            }
+            i++;
+        }while(left_brackets != 0);
+        return tree_out;
+    }
 }
