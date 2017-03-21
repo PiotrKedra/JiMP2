6cb8522b769f2a478d887e8848941c8374f925a4
PiotrKedra <piotr.k9750@gmail.com>
2017-03-21 11:08:49 +0100

    lab3

--- lab3/tinyurl/TinyUrl.cpp
+++ lab3/tinyurl/TinyUrl.cpp
@@ -7,17 +7,36 @@
 #include <string>
 #include <array>
 #include <memory>
+#include <iostream>
 
 namespace tinyurl {
-    struct TinyUrlCodec{
-        std::unique_ptr<TinyUrlCodec> yo;
-    };
 
     std::unique_ptr<TinyUrlCodec> Init() {
-
+        std::unique_ptr<TinyUrlCodec> init = std::make_unique<TinyUrlCodec>();
+        init->hash.fill('a');
+        return init;
     }
 
-    void NextHash(std::array<char, 6> *state) {}
+    void NextHash(std::array<char, 6> *state){
+        for(int i=5;i>=0;--i){
+            if((*state)[i] == 'z'){
+                (*state)[i] = '0';
+                continue;
+            }
+            else if((*state)[i] == '9'){
+                (*state)[i] = 'A';
+                break;
+            }
+            else if((*state)[i] == 'Z'){
+                (*state)[i] = 'a';
+                break;
+            }
+            else{
+                (*state)[i]+=1;
+                break;
+            }
+        }
+    }
 
     std::string Encode(const std::string &url, std::unique_ptr<TinyUrlCodec> *codec) {}
 
