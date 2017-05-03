73fcc5e60804da27f1bc87757f75026d34fe565a
PiotrKedra <piotr.k9750@gmail.com>
2017-05-03 23:06:56 +0200

    MovieSubtitles in progres

--- tests/lab8test/MovieSubtitlesTest.cpp
+++ tests/lab8test/MovieSubtitlesTest.cpp
@@ -21,7 +21,7 @@ class MovieSubtitlesTests : public ::testing::Test, MemLeakTest {
 };
 
 TEST_F(MovieSubtitlesTests, MicroDvdHasBaseClassOfMovieSubtitles) {
-  unique_ptr<MovieSubtitles> subs = make_unique<MicroDvdSubtitles>();
+  auto subs = make_unique<MicroDvdSubtitles>();
   stringstream in {"{0}{250}TEXT\n{260}{300}NEWLINE\n"};
   stringstream out;
   subs->ShiftAllSubtitlesBy(300, 25, &in, &out);
@@ -29,7 +29,7 @@ TEST_F(MovieSubtitlesTests, MicroDvdHasBaseClassOfMovieSubtitles) {
 }
 
 TEST_F(MovieSubtitlesTests, SubRipHasBaseClassOfMovieSubtitles) {
-  unique_ptr<MovieSubtitles> subs = make_unique<SubRipSubtitles>();
+  auto subs = make_unique<SubRipSubtitles>();
   stringstream in {"1\n00:05:54,555 --> 00:05:56,722\nText\n\n2\n00:06:06,433 --> 00:06:07,801\nNEWLINE\n"};
   stringstream out;
   EXPECT_NO_THROW(subs->ShiftAllSubtitlesBy(300, 24, &in, &out));
