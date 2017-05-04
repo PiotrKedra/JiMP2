05e12903c94cc0e19ca2aa4ab23716640c3d7674
PiotrKedra <piotr.k9750@gmail.com>
2017-05-04 17:06:45 +0200

    MicroDvd all completed

--- lab8/moviesubtitles/MovieSubtitles.cpp
+++ lab8/moviesubtitles/MovieSubtitles.cpp
@@ -16,9 +16,9 @@ namespace moviesubs{
         if(delay<0){
             throw NegativeFrameAfterShift(std::to_string(delay));
         }
-        if(!(in and out)){
+        /*if(!(in and out)){
             throw MovieSubtitlesException("Wrong files in or out");
-        }
+        }*/
         if(delay==0){
             return in->str();
         }
@@ -33,26 +33,36 @@ namespace moviesubs{
         bool EndBeforeStart = false;
         std::string EndBeforeStartStr = "";
         std::string exception = "";
-        std::regex pattern {R"(\{[0-9]+\}\{[0-9]+\}(...))"};
+        std::regex pattern {R"(((\{([0-9]+)\}){2})((.)+))"};
+        std::smatch matches;
         for(int i=0;i<input.size();++i){
-            if(input[i]=='{' and after_brackets){
+            if(after_brackets){
+                if(input[i]!='{'){
+                    throw InvalidSubtitleLineFormat("Wrong line format at line: "+line);
+                }
                 ++i;
                 while (input[i]!='}'){
-                    first_frame_str+=input[i];
+                    if(input[i]>= 48 and input[i]<=57) first_frame_str+=input[i];
                     ++i;
                 }
+                if(input[i]!='}' or input[i+1]!='{'){
+                    throw InvalidSubtitleLineFormat("Wrong line format at line: "+line);
+                }
                 i+=2;
-                while (input[i]!='}'){
-                    second_frame_str+=input[i];
+                while (input[i]!='}' and input[i]!='\n'){
+                    if(input[i]>= 48 and input[i]<=57) second_frame_str+=input[i];
                     ++i;
+                }
+                if(first_frame_str.size()==0 or second_frame_str.size()==0){
+                    throw InvalidSubtitleLineFormat("Wrong line format at line: "+line);
                 }
                 if(std::stoi(first_frame_str)>=std::stoi(second_frame_str)){
                     EndBeforeStart=true; // it's first step of throw SubtitleEndBeforeStart exception, if(EndBeforeStart)...
                 }
+                exception='{' + first_frame_str + '}' + '{' + second_frame_str + '}';
                 first_frame = std::stoi(first_frame_str) + DelayPerFrame;
                 second_frame = std::stoi(second_frame_str) + DelayPerFrame;
                 output += '{' + std::to_string(first_frame) + '}' + '{' + std::to_string(second_frame) + '}';
-                exception='{' + first_frame_str + '}' + '{' + second_frame_str + '}';
                 first_frame_str = "";
                 second_frame_str = "";
                 after_brackets=false;
@@ -64,16 +74,14 @@ namespace moviesubs{
             }
             if(input[i]=='\n'){
                 ++line;
+                if(!std::regex_match(exception, matches, pattern)){
+                    throw InvalidSubtitleLineFormat("Wrong line format at line: "+line);
+                }
                 if(EndBeforeStart){
                     std::string message = "At line " + std::to_string(line) + ": " + exception;
                     throw SubtitleEndBeforeStart(message, line);
                 }
                 after_brackets= true;
-                std::smatch matches;
-                /*if(!std::regex_match(exception, matches, pattern)){
-                    std::string message = "At line " + std::to_string(line) + ": " + exception;
-                    throw InvalidSubtitleLineFormat(message);
-                }*/
                 exception = "";
             }
             if(i==input.size()-1 and input[i]!='\n'){
--- lab8/moviesubtitles/main.cpp
+++ lab8/moviesubtitles/main.cpp
@@ -10,17 +10,21 @@
 using namespace moviesubs;
 
 int main(){
-    auto subs = std::make_unique<MicroDvdSubtitles>();
+    /*auto subs = std::make_unique<MicroDvdSubtitles>();
     std::stringstream in {"{0}{250}TEXT\n{260}{300}NEWLINE\n"};
     std::stringstream out;
     subs->ShiftAllSubtitlesBy(300, 25, &in, &out);
 
-    std::regex pattern {R"(\{[0-9]+\}\{[0-9]+\}(...)+)"};
-    std::string str = "{0}{250}TExt";
+    std::regex pattern {R"(((\{([0-9]+)\}){2})((.)+))"};
+    std::string str = "{0123}{x}TEXT";
     std::smatch matches;
     if(std::regex_match(str, matches, pattern)){
         std::cout << str;
-    }
+    }*/
+
+    std::string a="";
+
+    std::cout << std::stoi(a) << " tu co jestt";
 
 
     return 0;
