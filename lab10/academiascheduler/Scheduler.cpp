d839f6aa8f1adb54435583e2e814eb7d82c3f4cd
PiotrKedra <piotr.k9750@gmail.com>
2017-05-20 19:43:22 +0200

    virtual ...)=0;

--- lab10/academiascheduler/Scheduler.cpp
+++ lab10/academiascheduler/Scheduler.cpp
@@ -71,20 +71,4 @@ namespace academia {
 
         return p;
     }
-
-
-
-
-
-
-
-
-
-
-
-    Schedule Scheduler::PrepareNewSchedule(const std::vector<int> &rooms, const std::map<int,
-            std::vector<int>> &teacher_courses_assignment, const std::map<int, std::set<int>> &courses_of_year,
-                                           int n_time_slots) {
-        return Schedule();
-    }
 }
