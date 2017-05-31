6383778d8d981ecb3e3c468a490e098b655448b4
PiotrKedra <piotr.k9750@gmail.com>
2017-05-31 21:36:54 +0200

    sth

--- lab9/academiaserializer/main.cpp
+++ lab9/academiaserializer/main.cpp
@@ -2,3 +2,23 @@
 // Created by piotr on 04.05.2017.
 //
 
+#include <memory>
+#include <iostream>
+
+
+std::unique_ptr<int> GiveMeItForASec(std::unique_ptr<int> p) {
+    ++(*p);
+    return p;
+}
+
+int main(){
+    std::unique_ptr<int> value=std::make_unique<int>(3);
+    std::cout << *value;
+
+    value=GiveMeItForASec(move(value));
+    std::cout << *value;
+
+
+    return 0;
+}
+
