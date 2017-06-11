3d156fe21a6a6c7fc0afaae2f2ad7fa82b3466ce
PiotrKedra <piotr.k9750@gmail.com>
2017-06-11 15:33:43 +0200

    tree in prgores/pronlem with depth

--- lab12/tree/Tree.h
+++ lab12/tree/Tree.h
@@ -5,15 +5,27 @@
 #ifndef JIMP_EXERCISES_TREE_H
 #define JIMP_EXERCISES_TREE_H
 
-#import <memory>
+#include <memory>
+#include <iostream>
 
 namespace tree {
     template<class T>
+    class Node {
+    public:
+        Node(T value) : value_(value) {}
+
+        std::unique_ptr<Node<T>> left_ = nullptr;
+        std::unique_ptr<Node<T>> right_ = nullptr;
+        T value_;
+
+    };
+
+    template<class T>
     class Tree {
     public:
-        Tree() : left_(nullptr), right_(nullptr) {}
+        Tree() {}
 
-        Tree(T value) : value_(value), left_(nullptr), right_(nullptr), size_(1) {}
+        Tree(T value) : value_(value), size_(1) {}
 
         T Value() {
             return value_;
@@ -23,48 +35,57 @@ namespace tree {
             return this->Value() < right.Value();
         }
 
+        bool operator=(Tree right) {
+            return this->Value() = right.Value();
+        }
+
         void Insert(T new_value) {
             ++size_;
-            if (!this->value_) {
+            if (!this) {
                 value_ = new_value;
             } else {
                 Tree new_node(new_value);
-                Tree *next = this;
-                Tree *prev = this;
+                std::unique_ptr<Tree<T>> next = this;
+                std::unique_ptr<Tree<T>> prev = this;
                 while (next != nullptr) {
                     prev = next;
-                    if (new_node.Value() < next->Value()) {
+                    if (new_node.Value() < next->value_) {
                         next = next->left_;
                     } else {
                         next = next->right_;
                     }
                 }
-                if (prev->Value() < new_node.Value()) {
-                    prev->left_ = &new_node;
+                if (prev->value_ < new_node.Value()) {
+                    prev->left_ = new_node;
                 } else {
-                    prev->right_ = &new_node;
+                    prev->right_ = new_node;
                 }
             }
         }
-        int Size(){
+
+        int Size() {
             return size_;
         }
-        int Depth(){
-            if(this == nullptr) return 0;
-            else {
-                int lchild = this->left_->Depth();
-                int rchild = this->right_->Depth();
-                if(lchild <= rchild) return rchild+1;
-                else return lchild+1;
-            }
+
+        int Depth() {
+            return DepthPreorder(left_);
         }
-    public:
-        T value_;
-        Tree *left_, *right_;
-        int size_=0;
+
+    private:
+        int size_ = 1;
+        std::unique_ptr<Node<T>> left_ = nullptr;
+        std::unique_ptr<Node<T>> right_ = nullptr;
+        std::unique_ptr<Node<T>> value_;
+        int DepthPreorder(const std::unique_ptr<Node<T>> &root);
 
     };
-    
+
+    template<class T>
+    int Tree<T>::DepthPreorder(const std::unique_ptr<Node<T>> &root)  {
+        if (root != nullptr)
+            return 1 + std::max(this->DepthPreorder(root->left_), this->DepthPreorder(root->right_));
+        return 0;
+    }
 }
 
 
